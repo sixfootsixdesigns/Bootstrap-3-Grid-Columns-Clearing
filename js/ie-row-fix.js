fac7e320e3a7709b12ec6e22ddb8ce9166251637
James Olson <james@booj.com>
2013-08-21 11:38:46 -0600

    Update For Latest Bootstrap 3 Grid Changes
    removed col-X and added col-xs-X and col-md-X

--- js/ie-row-fix.js
+++ js/ie-row-fix.js
@@ -4,24 +4,30 @@ jQuery(document).ready(function($) {
 	function rowPolyfill() {
 		var w = $doc.width();
 		jQuery('.multi-columns-row > [class^="col-"]').removeClass('first-in-row');
-		if (w > 921) {
+		if (w > 1200) {
 			jQuery('.multi-columns-row > .col-lg-6:nth-child(2n + 3)').addClass('first-in-row');
 			jQuery('.multi-columns-row > .col-lg-4:nth-child(3n + 4)').addClass('first-in-row');
 			jQuery('.multi-columns-row > .col-lg-3:nth-child(4n + 5)').addClass('first-in-row');
 			jQuery('.multi-columns-row > .col-lg-2:nth-child(6n + 7)').addClass('first-in-row');
 			jQuery('.multi-columns-row > .col-lg-1:nth-child(12n + 13)').addClass('first-in-row');
-		} else if (w > 767) {
+		} else if (w >= 992) {
+			jQuery('.multi-columns-row > .col-md-6:nth-child(2n + 3)').addClass('first-in-row');
+			jQuery('.multi-columns-row > .col-md-4:nth-child(3n + 4)').addClass('first-in-row');
+			jQuery('.multi-columns-row > .col-md-3:nth-child(4n + 5)').addClass('first-in-row');
+			jQuery('.multi-columns-row > .col-md-2:nth-child(6n + 7)').addClass('first-in-row');
+			jQuery('.multi-columns-row > .col-md-1:nth-child(12n + 13)').addClass('first-in-row');
+		} else if (w >= 768) {
 			jQuery('.multi-columns-row > .col-sm-6:nth-child(2n + 3)').addClass('first-in-row');
 			jQuery('.multi-columns-row > .col-sm-4:nth-child(3n + 4)').addClass('first-in-row');
 			jQuery('.multi-columns-row > .col-sm-3:nth-child(4n + 5)').addClass('first-in-row');
 			jQuery('.multi-columns-row > .col-sm-2:nth-child(6n + 7)').addClass('first-in-row');
 			jQuery('.multi-columns-row > .col-sm-1:nth-child(12n + 13)').addClass('first-in-row');
 		} else {
-			jQuery('.multi-columns-row > .col-6:nth-child(2n + 3)').addClass('first-in-row');
-			jQuery('.multi-columns-row > .col-4:nth-child(3n + 4)').addClass('first-in-row');
-			jQuery('.multi-columns-row > .col-3:nth-child(4n + 5)').addClass('first-in-row');
-			jQuery('.multi-columns-row > .col-2:nth-child(6n + 7)').addClass('first-in-row');
-			jQuery('.multi-columns-row > .col-1:nth-child(12n + 13)').addClass('first-in-row');
+			jQuery('.multi-columns-row > .col-xs-6:nth-child(2n + 3)').addClass('first-in-row');
+			jQuery('.multi-columns-row > .col-xs-4:nth-child(3n + 4)').addClass('first-in-row');
+			jQuery('.multi-columns-row > .col-xs-3:nth-child(4n + 5)').addClass('first-in-row');
+			jQuery('.multi-columns-row > .col-xs-2:nth-child(6n + 7)').addClass('first-in-row');
+			jQuery('.multi-columns-row > .col-xs-1:nth-child(12n + 13)').addClass('first-in-row');
 		}
 	}	
 	rowPolyfill();
